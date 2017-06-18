2973dc879cfc7572a45e5b54ac81762fa16fbd08
gmli <guillaume.ludwig@gmail.com>
2017-06-18 22:53:45 +0200

    Test reset. Where could I place the W ?

--- keyboards/handwired/beponic/keymaps/default/keymap.c
+++ keyboards/handwired/beponic/keymaps/default/keymap.c
@@ -2,6 +2,7 @@
 #include "keymap_bepo.h"
 #include "keymap_french.h"
 #include "tmk_core/protocol/lufa/LUFA-git/LUFA/Drivers/Peripheral/TWI.h"
+#include "bootloader.h"
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -150,17 +151,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_BEPO] = { /* BEPO */ 
   { BP_DOLLAR,  BP_DQOT,  BP_LGIL,  BP_RGIL,  BP_LPRN,  BP_RPRN,  KC_DEL, BP_AT, BP_PLUS,  BP_MINUS, BP_SLASH, BP_ASTR,  BP_EQUAL, BP_PERCENT, KC_ESC },
-  { KC_TAB, BP_B,   BP_E_ACUTE, BP_P,   BP_O,   BP_E_GRAVE, KC_BSPC,  BP_DCRC,  BP_V,   BP_D,   BP_L,   BP_J,   BP_Z, BP_W , KC_RCTRL},
+  { KC_TAB, BP_B,   BP_E_ACUTE, BP_P,   BP_O,   BP_E_GRAVE, KC_BSPC,  BP_DCRC,  BP_V,   BP_D,   BP_L,   BP_J,   BP_Z, BP_W, KC_RCTRL},
   { TD(0), BP_A,   BP_U,   BP_I,   BP_E,   BP_COMMA, KC_ENTER, BP_C,   BP_T,   BP_S,   BP_R,   BP_N,   BP_M, KC_RSHIFT , KC_PGUP},
   { BP_ECRC, BP_A_GRAVE, BP_Y,   BP_X,   BP_DOT,   BP_K, KC_NO,  BP_APOS,  BP_Q,   BP_G,   BP_H,   BP_F,   BP_CCED, KC_UP, KC_PGDN},
-  { KC_NO, KC_NO, KC_LALT, M(1), KC_NO, KC_LCTL, MO(_FN), KC_NO, KC_SPC, KC_RALT, KC_HOME, KC_END, KC_LEFT, KC_DOWN, KC_RIGHT},
+  { M(21), KC_NO, KC_LALT, M(1), KC_NO, KC_LCTL, MO(_FN), KC_NO, KC_SPC, KC_RALT, KC_HOME, KC_END, KC_LEFT, KC_DOWN, KC_RIGHT},
   
  },
 
 
 [_FN] = { /* FN */
   { M(20), KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_DEL,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_PSCREEN },
-  { KC_TAB, KC_NO, KC_HOME,KC_UP, KC_END, KC_PGUP,KC_BSPC,                    KC_NO, BP_7,BP_8, BP_9, KC_NO,KC_NO, KC_NO, KC_RCTRL  },
+  { KC_TAB, KC_NO, KC_HOME,KC_UP, KC_END, KC_PGUP,KC_BSPC,                    KC_NO, BP_7,BP_8, BP_9, KC_NO,KC_NO, BP_W, KC_RCTRL  },
   { KC_LSHIFT, M(3), KC_LEFT,KC_DOWN, KC_RIGHT, KC_PGDN,KC_NO,             KC_NO, BP_4,BP_5, BP_6, KC_NO,KC_NO, KC_RSHIFT, KC_CAPSLOCK  },
   { M(9), MUV_IN, KC_NO,LCTL(BP_X), LCTL(BP_C), LCTL(BP_V),KC_NO,              BP_0 , BP_1,BP_2, BP_3, KC_NO,KC_NO, KC_NO, KC_NO  },
   { MU_ON, MU_OFF, KC_NO,KC_TRNS, KC_NO, KC_LCTL,KC_TRNS,                  KC_NO, RALT(KC_SPC),KC_NO, KC_NO, KC_NO,KC_HOME, KC_NO, KC_END  },
@@ -175,7 +176,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  //  { KC_D,    KC_LCTL, KC_LALT, KC_LGUI, KC_D, KC_SPC,  KC_SPC,  KC_D, KC_RGUI, KC_RALT, KC_RCTL, KC_D, KC_LEFT, KC_DOWN, KC_RGHT  },
  // },
 
- 
+
 
 
 };
@@ -242,6 +243,23 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
           }          // SEND_STRING ("auie");
           // layer_on(_FN);
           break;
+        case 21:
+          if (record->event.pressed) {
+              register_code(KC_LALT);
+              register_code(KC_TAB);
+          }
+          else {
+              unregister_code(KC_LALT);
+              unregister_code(KC_TAB);
+          }
+          break;
+        // case 42:
+        //   SEND_STRING ("Mathem");
+        //   clear_keyboard();
+        //   print("\n\nJump to bootloader... ");
+        //   wait_ms(250);
+        //   bootloader_jump(); // should not return
+        //   print("not supported.\n");
       }
     return MACRO_NONE;
 };
--- keyboards/handwired/beponic/rules.mk
+++ keyboards/handwired/beponic/rules.mk
@@ -46,13 +46,13 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 #   Atmel DFU loader 4096
 #   LUFA bootloader  4096
 #   USBaspLoader     2048
-OPT_DEFS += -DBOOTLOADER_SIZE=1024
+OPT_DEFS += -DBOOTLOADER_SIZE=2048
 
 # Build Options
 #   change to "no" to disable the options, or define them in the Makefile in 
 #   the appropriate keymap folder that will get included automatically
 #
-BOOTMAGIC_ENABLE ?= no       # Virtual DIP switch configuration(+1000)
+BOOTMAGIC_ENABLE ?= yes       # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE ?= yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE ?= yes       # Audio control and System control(+450)
 CONSOLE_ENABLE ?= no         # Console for debug(+400)
